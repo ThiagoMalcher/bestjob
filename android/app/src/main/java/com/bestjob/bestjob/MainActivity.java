804f3c7d5ffbc2ca856dfd963b99abd8ae15a7dc
TMALCHER <malcher.tm@gmail.com>
2021-12-18 11:58:35 -0300

    new files in project

---
+++ android/app/src/main/java/com/bestjob/bestjob/MainActivity.java
@@ -0,0 +1,6 @@
+package com.bestjob.bestjob;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import 'package:bestjob/Screens/HomeScreen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Best Job',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+        primaryColor: Color.fromARGB(255, 4, 125, 141)
+      ),
+        debugShowCheckedModeBanner: false,
+      home: HomeScreen()
+    );
+  }
+}
